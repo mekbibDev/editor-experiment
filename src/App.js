4f0bb585bea9f05eedc9b80cb0673f988e5bc237
kazumaneet <mekbibtare@gmail.com>
2022-03-24 20:45:09 +0300

    Add syntax highlighting

--- src/App.js
+++ src/App.js
@@ -1,146 +1,23 @@
 import React, { useCallback, useState, useMemo } from "react";
-import { createEditor, Editor, Transforms, Text } from 'slate'
-import { Slate, Editable, withReact } from 'slate-react'
-
-const customEditor = {
-  isBoldMarkActive(editor) {
-    const [match] = Editor.nodes(
-      editor,
-      {
-        match: n => n.bold === true,
-        universal: true
-      }
-    )
-
-    return !!match
-  },
-
-  isCodeBlockActive(editor) {
-    const [match] = Editor.nodes(
-      editor,
-      { match: n => n.type === 'code' }
-    )
-    return !!match
-  },
-  toggleCodeBlock(editor) {
-    const isActive = customEditor.isCodeBlockActive(editor)
-
-    Transforms.setNodes(
-      editor,
-      { type: isActive ? null : 'code' },
-      { match: n => Editor.isBlock(editor, n) }
-    )
-  },
-  toggleBoldMark(editor) {
-    const isActive = customEditor.isBoldMarkActive(editor)
-    Transforms.setNodes(
-      editor,
-      { bold: isActive ? null : true },
-      { match: n => Text.isText(n), split: true }
-    )
-  }
-}
-const CodeElement = (props) => {
-  return (
-    <pre {...props.attributes}>
-      <code>{props.children}</code>
-    </pre>
-  )
-}
-const DefaultElement = (props) => {
+import { RichTextEditor } from '@mantine/rte';
+const App = () => {
+  const initialValue =
+  '<p>Your initial <b>html value</b> or an empty string to init editor without value</p>'
+  const [value, onChange] = useState(initialValue);
+  const modules = useMemo(
+    () => ({
+      history: { delay: 2500, userOnly: true },
+      syntax: true,
+    }),
+    []
+  );
+  console.log(value)
   return (
     <>
-      <p {...props.attributes}> {props.children}</p>
+    
+    <RichTextEditor modules={modules} value={value} onChange={onChange} />
+    
     </>
-
-  )
-}
-const Leaf = props => {
-  return (
-    <span
-      {...props.attributes}
-      style={{ fontWeight: props.leaf.bold ? 'bold' : 'normal' }}
-    >
-      {props.children}
-    </span>
-  )
-}
-
-const App = () => {
-  const editor = useMemo(() => withReact(createEditor()), [])
-  const [value, setValue] = useState(
-    JSON.parse(localStorage.getItem('content')) || [
-      {
-        type: 'paragraph',
-        children: [{ text: 'A line of text in a paragraph.' }],
-      },
-    ]
-  )
-
-  const renderElement = useCallback(props => {
-    switch (props.element.type) {
-      case 'code':
-        return <CodeElement {...props} />
-      default:
-        return <DefaultElement {...props} />
-    }
-  }, [])
-
-  // Define a leaf rendering function that is memoized with `useCallback`.
-  const renderLeaf = useCallback(props => {
-    return <Leaf {...props} />
-  }, [])
-
-  return (
-    <Slate editor={editor} value={value} onChange={value => {
-      setValue(value)
-      const isAstChange = editor.operations.some(
-        op => 'set_selection' !== op.type
-      )
-      if (isAstChange) {
-        // Save the value to Local Storage.
-        const content = JSON.stringify(value)
-        localStorage.setItem('content', content)
-      }
-    }}>
-      <button onClick={(event) => {
-        event.preventDefault()
-        customEditor.toggleCodeBlock(editor)
-      }
-      }>
-        codeBlock
-      </button>
-
-      <button onClick={(event) => {
-        event.preventDefault()
-        customEditor.toggleBoldMark(editor)
-      }}>
-        bold
-      </button>
-      <Editable
-        renderElement={renderElement}
-        // Pass in the `renderLeaf` function.
-        renderLeaf={renderLeaf}
-        onKeyDown={event => {
-          if (!event.ctrlKey) {
-            return
-          }
-
-          switch (event.key) {
-            case '`':
-              event.preventDefault()
-              customEditor.toggleCodeBlock(editor)
-              break
-            case 'b':
-              event.preventDefault()
-              customEditor.toggleBoldMark(editor)
-              break
-            default:
-              return
-          }
-        }}
-      />
-    </Slate>
   )
 }
 export default App;
